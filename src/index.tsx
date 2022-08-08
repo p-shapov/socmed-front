b4cf30d17144aa9c80e4f6f9c3a78619a885e024
Pavel Shapov <pavel.shapov@metalamp.io>
2022-08-08 16:01:48 +0700

    Setup env

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'the-new-css-reset';
+import './shared/styles/global.scss';
+
+import { App } from './app';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
